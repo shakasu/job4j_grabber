bee75f7ee46981890dc3be75c9d44757483dc05d
shakasu <shakasuone@gmail.com>
2020-08-27 04:13:19 +0300

    Преобразование даты [#347852]

---
+++ src/main/java/ru/job4j/html/DateToString.java
@@ -0,0 +1,69 @@
+package ru.job4j.html;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+
+public class DateToString {
+    public Timestamp transform(String strDate) {
+        int year, month, day, hour, minute;
+
+        HashMap<String, Integer> months = new HashMap<>();
+        months.put("янв", 0);
+        months.put("фев", 1);
+        months.put("мар", 2);
+        months.put("апр", 3);
+        months.put("май", 4);
+        months.put("июн", 5);
+        months.put("июл", 6);
+        months.put("авг", 7);
+        months.put("сен", 8);
+        months.put("окт", 9);
+        months.put("ноя", 10);
+        months.put("дек", 11);
+
+        Calendar current = new GregorianCalendar();
+
+        if (strDate.split(" ").length == 4) {
+            day = Integer.parseInt(strDate.split(" ")[0]);
+            month = months.get(strDate.split(" ")[1]);
+            year = 100 + Integer.parseInt(strDate.split(" ")[2].split(",")[0]);
+            hour = Integer.parseInt(strDate.split(" ")[3].split(":")[0]);
+            minute = Integer.parseInt(strDate.split(" ")[3].split(":")[1]);
+        } else {
+            if (strDate.startsWith("сегодня") || strDate.startsWith("вчера")) {
+                hour = Integer.parseInt(strDate.split(" ")[1].split(":")[0]);
+                minute = Integer.parseInt(strDate.split(" ")[1].split(":")[1]);
+                if (strDate.startsWith("вчера")) {
+                    current.add(Calendar.DATE, -1);
+                }
+                day = current.get(Calendar.DAY_OF_MONTH);
+                year = current.get(Calendar.YEAR) - 1900;
+                month = current.get(Calendar.MONTH);
+            } else {
+                day = 0;
+                year = 0;
+                month = 0;
+                minute = 0;
+                hour = 0;
+            }
+        }
+        return new Timestamp(year, month, day, hour, minute, 0, 0);
+    }
+
+    public static void main(String[] args) throws Exception {
+        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
+        Elements dates = doc.select("td.altCol");
+        DateToString dateToString = new DateToString();
+        for (Element date : dates) {
+            System.out.println(dateToString.transform(date.text()));
+            System.out.println(date.text());
+        }
+    }
+}
