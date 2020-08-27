b47e01e78d4e7efb7ed21c11bbe493d1c4b4a865
shakasu <shakasuone@gmail.com>
2020-08-27 04:30:26 +0300

    Парсинг https://www.sql.ru/forum/job-offers/3 [#347848]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -7,19 +7,28 @@ import org.jsoup.select.Elements;
 
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".postslisttopic");
-        Elements dates = doc.select("td.altCol");
-        int di = 1;
-        for (int i = 0; i < row.size() && di < dates.size(); i++) {
-            Element href = row.get(i).child(0);
-            System.out.printf(
-                    "%s%n%s%n%s%n",
-                    href.attr("href"),
-                    href.text(),
-                    dates.get(i).text()
-            );
-            di += 2;
+        for (int k = 0; k < 6; k++) {
+            StringBuilder url = new StringBuilder("https://www.sql.ru/forum/job-offers");
+            if (k > 0) {
+                url.append("/");
+                url.append(k);
+            }
+            System.out.println(url.toString());
+            Document doc = Jsoup.connect(url.toString()).get();
+            Elements row = doc.select(".postslisttopic");
+            Elements dates = doc.select("td.altCol");
+            int di = 1;
+            for (int i = 0; i < row.size() && di < dates.size(); i++) {
+                Element href = row.get(i).child(0);
+                System.out.printf(
+                        "%s%n%s%n%s%n%d%n",
+                        href.attr("href"),
+                        href.text(),
+                        dates.get(i).text(),
+                        i
+                );
+                di += 2;
+            }
         }
     }
 }
