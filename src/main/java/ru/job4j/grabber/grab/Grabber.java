260ab11123a7efde90eee0120ddbf735e3bee904
shakasu <shakasuone@gmail.com>
2020-09-10 12:24:09 +0300

    Web. [#347854]

--- src/main/java/ru/job4j/grabber/grab/Grabber.java
+++ src/main/java/ru/job4j/grabber/grab/Grabber.java
@@ -89,8 +89,7 @@ public class Grabber implements Grab {
                     try (OutputStream out = socket.getOutputStream()) {
                         out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                         for (Post post : store.getAll()) {
-                            out.write(post.toString().getBytes());
-                            //out.write(post.getName().getBytes(StandardCharsets.UTF_16LE));
+                            out.write(post.toString().getBytes("cp1251"));
                             out.write(System.lineSeparator().getBytes());
                         }
                     } catch (IOException io) {
